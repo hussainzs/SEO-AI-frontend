bdba10413ff07a575bf1d844ec1f80ed7ad6864b
Hussain Zaidi <syedhussainzaidi2002@gmail.com>
2025-05-03 02:36:06 -0400

    added Space Grotesk font, removed default icon and added custom icon

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import './index.css'; // Importing global CSS styles
 
 function App() {
   return (
-    <div className="text-red-500">
-      <h1>Hello, Tailwind CSS!</h1>
+    // increase the font
+    <div className="text-red-500 text-3xl font-bold">
+      <h1 className="border-2 p-2.5">Hello, Tailwind CSS!</h1>
     </div>
   );
 }
